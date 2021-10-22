e93103809eac1283630508051d125bbfa40a6764
Nick Ufer <nick@ufer.dev>
2021-10-22 11:20:26 +0200

    fix: license header

--- plugins/modules/dns.py
+++ plugins/modules/dns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2021, Nick Ufer <nu@inwx.de>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# Copyright: (c) 2021, INWX <developer@inwx.de>
+# MIT License (see https://opensource.org/licenses/MIT)
 
 from __future__ import absolute_import, division, print_function
 
